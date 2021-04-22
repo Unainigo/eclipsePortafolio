2491f1d3c72145bb05f6bdecfad6b8288d670629
Unai Iñigo <unainigo1@gmail.com>
2021-04-23 00:21:35 +0200

    segundo subida de prueba con cambios

--- primeraPrueba/primera/__init__.py
+++ primeraPrueba/primera/__init__.py
@@ -4,7 +4,10 @@ while datos != " ":
     print("introduzca el nombre de sus amigos")
     datos = input()
     nombreAmigos.append(datos)
-nombreAmigos.remove(datos)
+if datos == " ":
+    nombreAmigos.remove(datos)
+else:
+    print("En el array solo hay datos de nombres")
 for amigos in nombreAmigos:
     print("el nombre de tus amigos es: " + amigos)
     
